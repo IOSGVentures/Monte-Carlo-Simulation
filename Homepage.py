a7cf0871cf7d1e7633df8120e86a3e6956c4c9b0
Darko995 <53577768+Darko995@users.noreply.github.com>
2023-06-08 11:59:08 +0200

    Add files via upload

---
+++ Homepage.py
@@ -0,0 +1,54 @@
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+import requests
+import json
+import matplotlib
+from matplotlib import pyplot as plt
+# Set the maximum number of open figures to 30
+matplotlib.rcParams['figure.max_open_warning'] = 30
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import datetime 
+import matplotlib.dates as mdates
+import pickle
+from pathlib import Path
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+from pycoingecko import CoinGeckoAPI
+cg = CoinGeckoAPI()
+
+st.set_page_config(page_title="Token Vesting Management Homepage", page_icon="🧐", layout="wide")
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+# --- USER AUTHENTICATION ---
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status is False:
+    st.error('Username/password is incorrect')
+elif authentication_status is None:
+    st.warning('Please enter your username and password')
+elif authentication_status:
+    authenticator.logout('Logout', 'main', key='unique_key')
+    st.write(f'Welcome *{name}*')
+    
+    # ---- SIDEBAR ----
+    authenticator.logout("Logout", "sidebar")
+    st.sidebar.title(f"Welcome {name}")
+    
+    
+    # ---- MAINPAGE ----
+    st.title(":bar_chart: Token Vesting Management Homepage")
+    st.markdown("##")
+    
