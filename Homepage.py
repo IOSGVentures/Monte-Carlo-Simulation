1d0534b43dcf2152d0e7eb7b54ffaefd3d2e11ba
Darko995 <53577768+Darko995@users.noreply.github.com>
2023-06-10 09:56:11 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -51,7 +51,7 @@ elif authentication_status:
     st.write('Welcome to IOSG token vesting management web application!')
     st.write('At this Application, we have a way to easily manage our acquired tokens.')
     st.write('Our intuitive and user-friendly platform empowers you to effortlessly oversee all our invested projects in one centralized location, providing you with key parameters at your fingertips.') 
-    st.write('With our "invested projects" page dedicated to displaying comprehensive information about all our invested projects, you can easily keep track of important details such as project names, token quantities, vesting periods, ROI and much more.')
+    st.write('With our "PROJECTS" page dedicated to displaying comprehensive information about all our invested projects, you can easily keep track of important details such as project names, token quantities, vesting periods, ROI and much more.')
     st.write('This overview ensures that you have a clear understanding of our token allocation, enabling you to make informed decisions and effectively manage our portfolio.')
-    st.write('"next month Token Vesting" is designed specifically for token vesting. This page allows you to efficiently handle the process for the upcoming month.')
+    st.write('"NEXT MONTH" is designed specifically for token vesting. This page allows you to efficiently handle the process for the upcoming month.')
     
