a1d83f789f879722e3c3d6f71f2b2586cd1f87b1
Darko995 <53577768+Darko995@users.noreply.github.com>
2023-07-13 17:04:36 +0200

    Update 1_PROJECTS.py

--- pages/1_PROJECTS.py
+++ pages/1_PROJECTS.py
@@ -2776,9 +2776,19 @@ elif authentication_status:
                     if checkbox_value:
                         selected_projects.append(checkbox_labels[index])
         submitted = st.form_submit_button('Submit')
-
+    
     #if submitted:
         # Display charts for selected projects
+        st.markdown(
+    """
+<style>
+[data-testid="stMetricLabel"] {
+    font-size: 100px;
+}
+</style>
+""",
+    unsafe_allow_html=True,
+)
         for project in selected_projects:
 
             st.header(f"Here's Token Vesting Schedule for {project.capitalize()}!")
