bb2ae3a538c7c7069bc5b6fcc12524e971ac8ab6
DarkoIOSG <118198921+DarkoIOSG@users.noreply.github.com>
2023-07-28 19:41:48 +0200

    Update 1_BTC_BASED_SIMULATION.py

--- pages/1_BTC_BASED_SIMULATION.py
+++ pages/1_BTC_BASED_SIMULATION.py
@@ -172,7 +172,7 @@ elif authentication_status:
         project_id = st.text_input('Enter the project ID from Token Terminal',key='1')
         sigma_btc = st.number_input('Enter the sigma-implied volatility from in the money option -  deribit',key='2')
         mean = st.number_input('Enter the mean-risk-neutral assumption underpinning option pricing models',key='3')
-        price_levels_str = st.number_input('Enter the price levels',key='4')
+        price_levels_str = st.text_input('Enter the price levels',key='4')
         price_levels = [int(s) for s in price_levels_str.split(',')]
         period = st.number_input('Enter the number of days',key='5')
         
