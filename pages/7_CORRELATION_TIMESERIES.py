2c98ffa6469e6e789b1b0996800001e29dbbde92
DarkoIOSG <118198921+DarkoIOSG@users.noreply.github.com>
2023-08-01 18:36:39 +0200

    Update 7_CORRELATION_TIMESERIES.py

--- pages/7_CORRELATION_TIMESERIES.py
+++ pages/7_CORRELATION_TIMESERIES.py
@@ -118,46 +118,14 @@ elif authentication_status:
 #with st.form("my_form",clear_on_submit=False):
     
 project_id = st.text_input('Enter the project ID from Token Terminal', key='1')
-
-# Define the parameter options
-parameter_options = [
-    "price",
-    "market_cap_fully_diluted",
-    "token_trading_volume",
-    "tokenholders",
-    "tvl",
-    "trading_volume",
-    "fees",
-    "user_wau",
-    "active_developers",
-    "code_commits"
-]
-
-# Create checkboxes for the parameters
-par1 = st.checkbox("Price", key='2')
-par2 = st.checkbox("FDV", key='3')
-par3 = st.checkbox("Token trading volume", key='4')
-par4 = st.checkbox("Tokenholders", key='5')
-par5 = st.checkbox("TVL", key='6')
-par6 = st.checkbox("Trading volume", key='7')
-par7 = st.checkbox("Fees", key='8')
-par8 = st.checkbox("Weekly active users", key='9')
-par9 = st.checkbox("Active developers", key='10')
-par10 = st.checkbox("Code commits", key='11')
-start_date = st.date_input("Start date", key='12')
+par1 = st.text_input('Enter the first parameter (price,market_cap_fully_diluted,token_trading_volume,tokenholders,tvl,trading_volume,fees,user_wau,active_developers,code_commits)', key='2')
+par2 = st.text_input('Enter the second parameter (price,market_cap_fully_diluted,token_trading_volume,tokenholders,tvl,trading_volume,fees,user_wau,active_developers,code_commits)', key='3')
+start_date = st.date_input('Enter ')
 
 with st.form("monte_carlo_form"):
     if st.form_submit_button("Submit"):
-        # Get the selected parameters
-        selected_parameters = [parameter_options[i] for i, param in enumerate([par1, par2]) if param]
         
-        # Check if at least one parameter is selected
-        if not selected_parameters:
-            st.error("Please select at least one parameter.")
-        else:
-            # Now you can use the selected_parameters list in your function plot_rolling_correlation()
-            # For example: plot_rolling_correlation(project_id, selected_parameters[0], selected_parameters[1])
-            st.header(f"Here's Correlation timeseries between {selected_parameters[0]} and {selected_parameters[1]} for {project_id.capitalize()}!")
-            f = plot_rolling_correlation(project_id, selected_parameters[0], selected_parameters[1])
-            st.pyplot(f)
+        st.header(f"Here's Correlation timeseries between {par1} and {par2} for {project_id.capitalize()} starting from {start_date}!")
+        f = plot_rolling_correlation(project_id, par1, par2, start_date)
+        st.pyplot(f)
 
