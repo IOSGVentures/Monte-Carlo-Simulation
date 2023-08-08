2fee82a9524ee8725679f5c6da3036fc4e0eae89
DarkoIOSG <118198921+DarkoIOSG@users.noreply.github.com>
2023-08-08 10:27:19 +0200

    Update 9_TOKEN_PRICE_MOVING_AVERAGES.py

--- pages/9_TOKEN_PRICE_MOVING_AVERAGES.py
+++ pages/9_TOKEN_PRICE_MOVING_AVERAGES.py
@@ -63,7 +63,7 @@ elif authentication_status:
             df = df.T.dropna()
             return df
     
-        headers = {"Authorization": "Bearer 3365c8fd-ade3-410f-99e4-9c82d9831f0b"}
+        headers = {"Authorization": st.secrets["Authorization"]}
     
         project_ids = ['bitcoin','ethereum']
     
